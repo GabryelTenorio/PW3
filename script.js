9caeafdfd852deb97956a0c3f1154e7aad4d6d08
GabryelTenorio <209790669+GabryelTenorio@users.noreply.github.com>
2026-05-22 20:44:29 -0300

    Atualiza script de cadastro

--- script.js
+++ script.js
@@ -21,39 +21,53 @@ form.addEventListener("submit", async function(event) {
     const regexSenha = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&]).{8,}$/;
 
     if (!regexSenha.test(senha)) {
-        mensagem.innerHTML = "Senha fraca! Use letra, número e caractere especial.";
+        mensagem.innerHTML =
+        "Senha fraca! Use letra, número e caractere especial.";
         return;
     }
 
     // JSON
     const dados = {
+
         nome: nome,
         email: email,
         senha: senha
+
     };
 
     console.log("JSON enviado:");
     console.log(JSON.stringify(dados));
 
-    mensagem.innerHTML = "Enviando para servidor...";
+    mensagem.innerHTML =
+    "Enviando para servidor...";
 
     // SIMULAÇÃO ASSÍNCRONA DO SERVIDOR
     await simularServidor(dados);
+
 });
 
 // FUNÇÃO QUE SIMULA O PHP/SERVIDOR
 function simularServidor(dados) {
+
     return new Promise((resolve) => {
+
         console.log("Servidor recebeu:");
         console.log(dados);
 
+        // TEMPO DE 10 SEGUNDOS
         setTimeout(() => {
-            mensagem.innerHTML = "Cadastro realizado com sucesso!";
 
-            // REDIRECIONA PARA A PÁGINA DE SUCESSO
-            window.location.href = "sucesso.html";
+            mensagem.innerHTML =
+            "Cadastro realizado com sucesso!";
+
+            // REDIRECIONA
+            window.location.href =
+            "sucesso.html";
 
             resolve();
-        }, 1000);
+
+        }, 10000);
+
     });
+
 }
